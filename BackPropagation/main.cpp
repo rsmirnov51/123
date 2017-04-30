a22fb0b35a5e4f9ce20eb839d8fd0d3f2f8520b9
rsmirnov51 <roman-smr@mail.ru>
2017-04-30 22:27:07 +0300

    Update main.cpp

--- BackPropagation/main.cpp
+++ BackPropagation/main.cpp
@@ -1,7 +1,32 @@
+#include <vector>
 #include <iostream>
-#include "neural.h"
-#include "out.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <algorithm>
+#include <sstream>
 using namespace std;
+
+
+class neural
+{
+public:
+	neural(int NT, int MT, int JT, double etaT, double allowableErrorT, int maxEpochT, vector<double> xT, vector<double> tT);
+	neural();
+	vector<double> t, x;
+
+	vector<vector<vector<double> > > w;
+	vector<double> error;
+	vector<vector<double> > outY;
+	double allowableError;
+	int N, M, J, maxEpoch;
+	double eta;
+
+	double fa(double net);
+	double dfa(double net);
+	bool teaching();
+	void resetW();
+};
 
 
 int main()
